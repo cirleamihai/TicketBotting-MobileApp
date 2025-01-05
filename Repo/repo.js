4589366a34412e3b90bf548ef7241532d1a9fd76
krlea_dev <alexcirlea@hotmail.com>
2025-01-05 22:48:30 +0200

    Added in Memory 10 events

--- Repo/repo.js
+++ Repo/repo.js
@@ -1,4 +1,5 @@
 import {addEvent, deleteEvent, updateEvent, getEvents} from "../Api/eventsAPI";
+import Event from "../Entities/events";
 
 class EventsRepo {
     events = [];
@@ -28,5 +29,20 @@ class EventsRepo {
     }
 }
 
+const sampleEvents = [
+    new Event(null, "Rock Night", "A night of rock music.", "The Rockers", new Date(2025, 0, 15)),
+    new Event(null, "Jazz Evening", "An intimate evening with jazz classics.", "Smooth Jazz Trio", new Date(2025, 1, 20)),
+    new Event(null, "Indie Vibes", "Indie artists showcasing their best work.", "IndieSound Collective", new Date(2025, 2, 10)),
+    new Event(null, "Hip-Hop Beats", "The best hip-hop show of the year.", "DJ Flow", new Date(2025, 3, 5)),
+    new Event(null, "Classical Gala", "An orchestral night of classical music.", "Philharmonic Symphony", new Date(2025, 4, 25)),
+    new Event(null, "Pop Explosion", "The biggest pop hits performed live.", "Pop Stars United", new Date(2025, 5, 12)),
+    new Event(null, "Acoustic Night", "A laid-back night of acoustic covers.", "Acoustic Wanderers", new Date(2025, 6, 18)),
+    new Event(null, "EDM Festival", "An electrifying night of EDM music.", "DJ Electro", new Date(2025, 7, 30)),
+    new Event(null, "Soul Sessions", "A soulful night of smooth vocals.", "Soul Singers Crew", new Date(2025, 8, 8)),
+    new Event(null, "Blues Legends", "Blues legends performing live.", "Blues Revival Band", new Date(2025, 9, 20))
+];
+
 const eventsRepo = new EventsRepo();
+eventsRepo.events = sampleEvents;
+
 export default eventsRepo;
