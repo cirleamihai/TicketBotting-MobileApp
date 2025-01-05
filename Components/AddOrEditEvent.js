7bd09a3641258500c079a6c09e191b2dd8af1df7
krlea_dev <alexcirlea@hotmail.com>
2025-01-05 15:22:04 +0200

    Added Add Page Formatting

--- Components/AddOrEditEvent.js
+++ Components/AddOrEditEvent.js
@@ -3,8 +3,8 @@ import {useState} from "react";
 import {DateTimePickerAndroid} from "@react-native-community/datetimepicker";
 
 export default function AddOrEditEvent(props) {
+    let isEmptyEvent = props.event.is_empty();
     const [event, setEvent] = useState(props.event);
-    const [openDatePicker, setOpenDatePicker] = useState(false);
     const [date, setDate] = useState(event.date);
 
     const handleSave = () => {
@@ -19,7 +19,7 @@ export default function AddOrEditEvent(props) {
             mode: 'date',
             display: 'default',
             onChange: (event, selectedDate) => {
-                if (selectedDate) {
+                if (event.type === "set" && selectedDate) {
                     setDate(selectedDate);
                     setEvent({...event, date: selectedDate});
                 }
@@ -29,33 +29,46 @@ export default function AddOrEditEvent(props) {
 
     return (
         <View style={styles.appLayover}>
-            <TextInput
-                placeholder={"Event Name"}
-                value={event.eventName}
-                onChangeText={(text) => setEvent({...event, eventName: text})}
-            />
-            <TextInput
-                placeholder={"Description"}
-                value={event.description}
-                onChangeText={(text) => setEvent({...event, description: text})}
-            />
-            <TextInput
-                placeholder={"Artist"}
-                value={event.artist}
-                onChangeText={(text) => setEvent({...event, artist: text})}
-            />
-            <View>
-                <Pressable onPress={openDatePickerSelector}>
-                    <TextInput
-                        placeholder={"Select a date"}
-                        value={date ? date.toDateString(): ''}
-                        editable={false}
-                    />
-                </Pressable>
+            <View style={styles.batchOf2Inputs}>
+                <TextInput
+                    placeholder={"Event Name"}
+                    value={event.eventName}
+                    onChangeText={(text) => setEvent({...event, eventName: text})}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder={"Description"}
+                    value={event.description}
+                    onChangeText={(text) => setEvent({...event, description: text})}
+                    style={styles.input}
+                />
             </View>
-            <View>
-                <Button title={"Save Event"} onPress={handleSave}/>
-                <Button title={"Cancel"} onPress={props.onClose}/>
+            <View style={[styles.batchOf2Inputs]}>
+                <View style={styles.input}>
+                    <TextInput
+                        placeholder={"Artist"}
+                        value={event.artist}
+                        onChangeText={(text) => setEvent({...event, artist: text})}
+                    />
+                </View>
+                <View style={styles.input}>
+                    <Pressable onPress={openDatePickerSelector}>
+                        <TextInput
+                            placeholder={"Select a date"}
+                            value={date ? date.toDateString() : ''}
+                            editable={false}
+                        />
+                    </Pressable>
+                </View>
+            </View>
+
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title={"Save Event"} onPress={handleSave}/>
+                </View>
+                <View style={styles.button}>
+                    <Button title={"Cancel"} onPress={props.onClose}/>
+                </View>
             </View>
         </View>
     )
@@ -67,5 +80,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    batchOf2Inputs: {
+
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        justifyContent: 'space-between',
+        marginHorizontal: 10,
+        marginBottom: 10,
+        borderWidth: 0.5,
+        borderColor: "#cccccc",
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',  // Adds equal space between buttons
+
+    },
+    button: {
+        marginHorizontal: 20,
+        width: '35%',
+    }
 
 })
